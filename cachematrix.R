07d100ea4ffb8c1703cc5ac2dc5053397223153f
ebrule <ebrule@yahoo.com>
2014-06-22 15:53:47 -0400

    Updated with Programming Assignment 2 Changes

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse.
+## The function has 4 methods:
+##     set: sets the value of an identity matrix in the global environment - used to initialize global veraibles
+##     get: gets the value of an identity matrix from the global environment
+##     setCacheMatrix: set the value of a solved identity matrix in the global environment
+##     getCacheMatrix: get the value of a solved identity matrix from the global environment
+## Sample usage: 
+##    m <- makeCacheMatrix()
+##    m$set(matrix(c(0,2,2,0),2,2))
+##    m$get()
+##    m$setCacheMatrix(a)
+##    M$getCacheMatrix()
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setCacheMatrix <- function(solve) m <<- solve
+  getCacheMatrix <- function() m
+  list(set = set, get = get,
+       setCacheMatrix = setCacheMatrix,
+       getCacheMatrix = getCacheMatrix)
 }
 
 
-## Write a short comment describing this function
-
+## This function computes the inverse of the special "matrix" 
+##     returned by makeCacheMatrix(). 
+##If the inverse has already been calculated (i.e. the cached value is not null), 
+##     then cacheSolve will retrieve the inverse from the cache.
+## 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getCacheMatrix()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setCacheMatrix(m)
+  m
 }
